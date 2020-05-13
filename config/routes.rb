ab37b897bdac372a565f50cecec90a7d93489c1a
vagrant <vagrant@localhost.localdomain>
2020-05-13 07:45:08 +0000

    finish

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+
+resources :books
+root :to  => 'books#top'
+  # get '/books' => 'books#index'
+  # post '/books' => 'books#create'
+  # get '/books/:id' =>'books#show',as:'book'
+  # get '/books/:id/edit' =>'books#edit',as:'edit_book'
+  # patch '/books/:id' =>'books#update',as:'update_book'
+  # delete '/books/:id' => 'books#destroy',as: 'destroy_book'
+end
